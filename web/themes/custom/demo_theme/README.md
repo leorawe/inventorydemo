db37d5f782ea90d89a0a9a4bbb4b99e99f682d31
Leora Wenger <leorawenger@gmail.com>
2024-08-09 09:34:38 -0400

    algoliasearch, instantsearch, dotenv for appid, api key.

--- web/themes/custom/demo_theme/README.md
+++ web/themes/custom/demo_theme/README.md
@@ -1,1 +1,4 @@
 demo_theme theme, generated from starterkit_theme. Additional information on generating themes can be found in the [Starterkit documentation](https://www.drupal.org/docs/core-modules-and-themes/core-themes/starterkit-theme).
+
+npm run start  
+npm run build  
--- web/themes/custom/demo_theme/js/src/index.jsx
+++ web/themes/custom/demo_theme/js/src/index.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import algoliasearch from 'algoliasearch/lite';
+import instantsearch from 'instantsearch.js';
+import { searchBox, hits } from 'instantsearch.js/es/widgets';
 
-ReactDOM.render(
-  <h1>Need to add the input box for search here</h1>,
-  document.getElementById('react-app')
-);
+const searchClient = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
+
+const search = instantsearch({
+  indexName: 'dev_content',
+  searchClient,
+});
+
+search.addWidgets([
+  searchBox({
+    container: "#searchbox"
+  }),
+
+  hits({
+    container: "#hits"
+  })
+]);
+
+search.start();
