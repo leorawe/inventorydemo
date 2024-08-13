9bd15258161df135e9c3261aa27867510674ec60
Leora Wenger <leorawenger@gmail.com>
2024-08-13 09:13:07 -0400

    Autocomplete dropdown, packages.

--- web/modules/custom/demo_search/js/src/components/Autocomplete.tsx
+++ web/modules/custom/demo_search/js/src/components/Autocomplete.tsx
@@ -13,18 +13,12 @@ import {
 import { createRoot, Root } from 'react-dom/client';
 
 import {
-  useHierarchicalMenu,
   usePagination,
   useSearchBox,
 } from 'react-instantsearch';
 import { autocomplete } from '@algolia/autocomplete-js';
-// import { createLocalStorageRecentSearchesPlugin } from '@algolia/autocomplete-plugin-recent-searches';
-// import { createQuerySuggestionsPlugin } from '@algolia/autocomplete-plugin-query-suggestions';
+import { createQuerySuggestionsPlugin } from '@algolia/autocomplete-plugin-query-suggestions';
 import { debounce } from '@algolia/autocomplete-shared';
-
-import {
-  INSTANT_SEARCH_INDEX_NAME,
-} from '../constants';
 
 import '@algolia/autocomplete-theme-classic';
 
@@ -48,9 +42,7 @@ export function Autocomplete({
   const rootRef = useRef<HTMLElement | null>(null);
 
   const { query, refine: setQuery } = useSearchBox();
-  // const { items: categories, refine: setCategory } = useHierarchicalMenu({
-  //   attributes: INSTANT_SEARCH_HIERARCHICAL_ATTRIBUTES,
-  // });
+
   const { refine: setPage } = usePagination();
 
   const [instantSearchUiState, setInstantSearchUiState] =
@@ -62,144 +54,44 @@ export function Autocomplete({
 
   useEffect(() => {
     setQuery(instantSearchUiState.query);
-    // instantSearchUiState.category && setCategory(instantSearchUiState.category);
     setPage(0);
   }, [instantSearchUiState]);
 
-  // const currentCategory = useMemo(
-  //   () => categories.find(({ isRefined }) => isRefined)?.value,
-  //   [categories]
-  // );
+  const plugins = useMemo(() => {
+    const querySuggestions = createQuerySuggestionsPlugin({
+      searchClient,
+      indexName: 'instant_search_demo_query_suggestions',
+      getSearchParams({ state }) {
+        return { hitsPerPage: state.query ? 5 : 10 };
+      },
+      transformSource({ source }) {
+        return {
+          ...source,
+          sourceId: 'querySuggestionsPlugin',
+          onSelect({ item }) {
+            setInstantSearchUiState({
+              query: item.query,
+              category: item.__autocomplete_qsCategory || '',
+            });
+          },
+          getItems(params) {
+            if (!params.state.query) {
+              return [];
+            }
 
-  // const plugins = useMemo(() => {
-  //   // const recentSearches = createLocalStorageRecentSearchesPlugin({
-  //   //   key: 'instantsearch',
-  //   //   limit: 3,
-  //   //   transformSource({ source }) {
-  //   //     return {
-  //   //       ...source,
-  //   //       onSelect({ item }) {
-  //   //         setInstantSearchUiState({
-  //   //           query: item.label,
-  //   //           category: item.category,
-  //   //         });
-  //   //       },
-  //   //     };
-  //   //   },
-  //   // });
-
-  //   // const querySuggestionsInCategory = createQuerySuggestionsPlugin({
-  //   //   searchClient,
-  //   //   indexName: INSTANT_SEARCH_QUERY_SUGGESTIONS,
-  //   //   getSearchParams() {
-  //   //     return recentSearches.data!.getAlgoliaSearchParams({
-  //   //       hitsPerPage: 3,
-  //   //       facetFilters: [
-  //   //         `${INSTANT_SEARCH_INDEX_NAME}.facets.exact_matches.${INSTANT_SEARCH_HIERARCHICAL_ATTRIBUTES[0]}.value:${currentCategory}`,
-  //   //       ],
-  //   //     });
-  //   //   },
-  //   //   transformSource({ source }) {
-  //   //     return {
-  //   //       ...source,
-  //   //       sourceId: 'querySuggestionsInCategoryPlugin',
-  //   //       onSelect({ item }) {
-  //   //         setInstantSearchUiState({
-  //   //           query: item.query,
-  //   //           category: item.__autocomplete_qsCategory,
-  //   //         });
-  //   //       },
-  //   //       getItems(params) {
-  //   //         if (!currentCategory) {
-  //   //           return [];
-  //   //         }
-
-  //   //         return source.getItems(params);
-  //   //       },
-  //   //       templates: {
-  //   //         ...source.templates,
-  //   //         header({ items }) {
-  //   //           if (items.length === 0) {
-  //   //             return <Fragment />;
-  //   //           }
-
-  //   //           return (
-  //   //             <Fragment>
-  //   //               <span className="aa-SourceHeaderTitle">
-  //   //                 In {currentCategory}
-  //   //               </span>
-  //   //               <span className="aa-SourceHeaderLine" />
-  //   //             </Fragment>
-  //   //           );
-  //   //         },
-  //   //       },
-  //   //     };
-  //   //   },
-  //   // });
-
-  //   // const querySuggestions = createQuerySuggestionsPlugin({
-  //   //   searchClient,
-  //   //   indexName: INSTANT_SEARCH_QUERY_SUGGESTIONS,
-  //   //   getSearchParams() {
-  //   //     if (!currentCategory) {
-  //   //       return recentSearches.data!.getAlgoliaSearchParams({
-  //   //         hitsPerPage: 6,
-  //   //       });
-  //   //     }
-
-  //   //     return recentSearches.data!.getAlgoliaSearchParams({
-  //   //       hitsPerPage: 3,
-  //   //       facetFilters: [
-  //   //         `${INSTANT_SEARCH_INDEX_NAME}.facets.exact_matches.${INSTANT_SEARCH_HIERARCHICAL_ATTRIBUTES[0]}.value:-${currentCategory}`,
-  //   //       ],
-  //   //     });
-  //   //   },
-  //   //   categoryAttribute: [
-  //   //     INSTANT_SEARCH_INDEX_NAME,
-  //   //     'facets',
-  //   //     'exact_matches',
-  //   //     INSTANT_SEARCH_HIERARCHICAL_ATTRIBUTES[0],
-  //   //   ],
-  //   //   transformSource({ source }) {
-  //   //     return {
-  //   //       ...source,
-  //   //       sourceId: 'querySuggestionsPlugin',
-  //   //       onSelect({ item }) {
-  //   //         setInstantSearchUiState({
-  //   //           query: item.query,
-  //   //           category: item.__autocomplete_qsCategory || '',
-  //   //         });
-  //   //       },
-  //   //       getItems(params) {
-  //   //         if (!params.state.query) {
-  //   //           return [];
-  //   //         }
-
-  //           return source.getItems(params);
-  //         },
-  //         templates: {
-  //           ...source.templates,
-  //           header({ items }) {
-  //             if (!currentCategory || items.length === 0) {
-  //               return <Fragment />;
-  //             }
-
-  //             return (
-  //               <Fragment>
-  //                 <span className="aa-SourceHeaderTitle">
-  //                   In other categories
-  //                 </span>
-  //                 <span className="aa-SourceHeaderLine" />
-  //               </Fragment>
-  //             );
-  //           },
-  //         },
-  //       };
-  //     },
-  //   });
-
-  //   return [recentSearches, querySuggestionsInCategory, querySuggestions];
-  // }, [currentCategory]);
+            return source.getItems(params);
+          },
+          templates: {
+            ...source.templates,
+            header() {
+              return <Fragment />;
+            }
+          }
+        }
+      }
+    });
+    return [querySuggestions];
+  }, [])
 
   useEffect(() => {
     if (!autocompleteContainer.current) {
@@ -211,6 +103,7 @@ export function Autocomplete({
       container: autocompleteContainer.current,
       initialState: { query },
       insights: true,
+      plugins,
       onReset() {
         setInstantSearchUiState({ query: '' });
       },
