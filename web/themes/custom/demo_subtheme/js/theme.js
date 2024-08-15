2898000b7322c965420471df4e36a05eeaca1ffb
Leora Wenger <leorawenger@gmail.com>
2024-08-15 09:17:59 -0400

    Demo Subtheme sub theme of Olivero.

---
+++ web/themes/custom/demo_subtheme/js/theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * demo_subtheme behaviors.
+ */
+
+(function ($, Drupal) {
+  "use strict";
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.demoSubtheme = {
+    attach: function (context, settings) {
+      // My custom js.
+    },
+  };
+})(jQuery, Drupal);
